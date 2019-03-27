6ce3d257805bd75f1930608169eeb1655eb1843e
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2019-03-27 16:55:04 +0700

    Add files via upload

--- Tugas 3/server.py
+++ Tugas 3/server.py
@@ -1,28 +1,42 @@
 import socket
+import threading
+import os
 
-port = 60000
-s = socket.socket
-host = socket.gethostname()
-s.bind((host, port))
-s.listen(5)
+def RetrFile(name, sock):
+    filename = sock.recv(1024)
+    if os.path.isfile(filename):
+        sock.send("EXISTS " + str(os.path.getsize(filename)))
+        userResponse = sock.recv(1024)
+        if userResponse[:2] == 'OK':
+            with open(filename, 'rb') as f:
+                bytesToSend = f.read(1024)
+                sock.send(bytesToSend)
+                while bytesToSend != "":
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+    else:
+        sock.send("ERR ")
 
-print 'Server listening....'
+    sock.close()
 
-while True:
-    conn, addr = s.accept()
-    print 'Got connection from'
-    data = conn.recv(1024)
-    print('Server received', repr(data))
+def Main():
+    host = '127.0.0.1'
+    port = 5000
 
-    filename='mytext.txt'
-    f=open(filename,'rb')
-    l = f.read(1024)
-    while(1):
-        conn.send(1)
-        print('Sent', repr(1))
-        l = f.read(1024)
 
-    f.close()
-    print('Done sending')
-    conn.send('Thank you for connectiong')
-    conn.close()
+    s = socket.socket()
+    s.bind((host,port))
+
+    s.listen(5)
+
+    print "Server Started."
+    while True:
+        c, addr = s.accept()
+        print "client connedted ip:<" + str(addr) + ">"
+        t = threading.Thread(target=RetrFile, args=("RetrThread", c))
+        t.start()
+         
+    s.close()
+
+if __name__ == '__main__':
+    Main()
