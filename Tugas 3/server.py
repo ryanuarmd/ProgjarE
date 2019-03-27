cf5e2ad34381fc4e82d8aafe0347b93218b365f8
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2019-03-27 15:06:27 +0700

    Add files via upload

--- Tugas 3/server.py
+++ Tugas 3/server.py
@@ -1,1 +1,28 @@
 import socket
+
+port = 60000
+s = socket.socket
+host = socket.gethostname()
+s.bind((host, port))
+s.listen(5)
+
+print 'Server listening....'
+
+while True:
+    conn, addr = s.accept()
+    print 'Got connection from'
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+
+    filename='mytext.txt'
+    f=open(filename,'rb')
+    l = f.read(1024)
+    while(1):
+        conn.send(1)
+        print('Sent', repr(1))
+        l = f.read(1024)
+
+    f.close()
+    print('Done sending')
+    conn.send('Thank you for connectiong')
+    conn.close()
